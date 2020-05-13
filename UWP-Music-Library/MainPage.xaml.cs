c558af72c504f03210dc68bd607e948c09ef9e75
harveyx22 <52708955+harveyx22@users.noreply.github.com>
2020-05-12 19:44:42 -0700

    Added Play/Pause and Volume Features

--- UWP-Music-Library/MainPage.xaml.cs
+++ UWP-Music-Library/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UWP_Music_Library.Model;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,48 @@ namespace UWP_Music_Library
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<Song> Songs;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Songs = new ObservableCollection<Song>();
+            SongManager.GetAllSongs(Songs);
+
+            var song = Songs[0];
+            MyMediaElement.Source = new Uri(BaseUri, song.AudioFile);
+
+            PlayButton.Visibility = Visibility.Collapsed;
+            PauseButton.Visibility = Visibility.Visible;
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayButton.Visibility = Visibility.Collapsed;
+            PauseButton.Visibility = Visibility.Visible;
+            MyMediaElement.Play();            
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayButton.Visibility = Visibility.Visible;
+            PauseButton.Visibility = Visibility.Collapsed;
+            MyMediaElement.Pause();
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            MyMediaElement.Volume = (double)VolumeSlider.Value;
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ UWP-Music-Library/Model/SongManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWP_Music_Library.Model;
+
+namespace UWP_Music_Library.Model
+{
+    public static class SongManager
+    {
+        public static void GetAllSongs(ObservableCollection<Song> songs)
+        {
+            var allSongs = getSongs();
+            songs.Clear();
+            allSongs.ForEach(song => songs.Add(song));
+        }
+
+
+        private static List<Song> getSongs()
+        {
+            var songs = new List<Song>();
+            songs.Add(new Song("Remember Me As A Time Of Day", "Explosions in the Sky", "How Strange, Innocence"));
+            return songs;
+        }
+    }
+}
