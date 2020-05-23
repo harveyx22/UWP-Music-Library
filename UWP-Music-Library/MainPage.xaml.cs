45b03786abbc933febffa8dd4334fe25bee5cbc6
barnotas <61534987+barnotas@users.noreply.github.com>
2020-05-22 22:22:51 -0700

    i corrected errors

--- UWP-Music-Library/MainPage.xaml.cs
+++ UWP-Music-Library/MainPage.xaml.cs
@@ -14,14 +14,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.Storage;
-using Windows.Storage.Pickers;
-using Windows.Storage.Provider;
-using Windows.ApplicationModel;
-using System.Threading.Tasks;
-using Windows.Media.Core;
-using Windows.Media.Playback;
-using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -33,218 +25,118 @@ namespace UWP_Music_Library
     public sealed partial class MainPage : Page
     {
         private ObservableCollection<Song> Songs;
-        private List<Song> SongsList;
-        // private MediaPlayer mediaPlayer;
-        private MediaPlaybackList mediaPlaybackList;
 
         public MainPage()
         {
             this.InitializeComponent();
-
             Songs = new ObservableCollection<Song>();
             SongManager.GetAllSongs(Songs);
 
-            SongsList = Songs.ToList();
+            var song = Songs[0];
+            MyMediaElement.Source = new Uri(BaseUri, song.AudioFile);
 
-            mediaPlaybackList = new MediaPlaybackList();
-
-            // Initialize mediaplayer with playbacklist of all songs.
-            initializeMediaPlayer();
+            PlayButton.Visibility = Visibility.Collapsed;
+            PauseButton.Visibility = Visibility.Visible;
         }
 
-        private void initializeMediaPlayer()
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
-            // Iterate through songlist, adding each to playbacklist.
-            foreach (Song music in SongsList)
+            PlayButton.Visibility = Visibility.Collapsed;
+            PauseButton.Visibility = Visibility.Visible;
+            MyMediaElement.Play();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayButton.Visibility = Visibility.Visible;
+            PauseButton.Visibility = Visibility.Collapsed;
+            MyMediaElement.Pause();
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            MyMediaElement.Volume = (double)VolumeSlider.Value;
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void SearchAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (String.IsNullOrEmpty(sender.Text)) goBack();
+
+            SongManager.GetAllSongs(Songs);
+            Suggestions = Songs.Where(p => p.Name.StartsWith(sender.Text)).Select(p => p.Name).ToList();
+            SearchAutoSuggestBox.ItemsSource = Suggestions;
+
+        }
+
+        private void goBack()
+        {
+            SongManager.GetAllSongs(Songs)
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+
+
+        private void SearchAutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            SongManager.GetSongsByArtistName(Songs, sender.Text);
+
+            BackButton.Visibility = Visibility.Visible;
+        }
+
+
+
+    }
+
+    private void MusicGridView_ItemClick(object sender, ItemClickEventArgs e)
+    {
+        var sound = (Song)e.ClickedItem;
+        MyMediaElement.Source = new Uri(this.BaseUri, Song.AudioFiles);
+    }
+
+    private async void MusicGridView_Drop(object sender, DragEventArgs e)
+    {
+        if (e.DataView.Contains(StandardDataFormats.StorageItems))
+        {
+            var items = await e.DataView.GetStorageItemsAsync();
+
+            if (items.Any())
             {
-                var mediaPlaybackItem = new MediaPlaybackItem(MediaSource.CreateFromUri(new Uri(BaseUri, music.AudioFile)));
-                mediaPlaybackList.Items.Add(mediaPlaybackItem);
-            }
-            mediaPlaybackList.MaxPlayedItemsToKeepOpen = 3;
+                var storageFile = items[0] as StorageFile;
+                var contentType = storageFile.ContentType;
 
-            MyMediaPlayer.Source = mediaPlaybackList;
-        }
+                StorageFolder folder = ApplicationData.Current.LocalFolder;
 
-        private void MusicListView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            // Obtain song object chosen from list.
-            var song = (Song)e.ClickedItem;
+                if (contentType == "audio/"mp3 || contentType == "audio/mp3")
+                    {
+                    StorageFile newFile = await storageFile.CopyAsync(folder, storageFile.Name, NameCollisionOption.GenerateUniqueName);
 
-            // Find index of song in song list.
-            int clickedIndex = SongsList.FindIndex(s => s.Name == song.Name);
-
-            // Get spliced list of chosen song to end of list, and add original beginning of list to the end of this new one. 
-            var filteredSongs = SongsList.Skip(clickedIndex);
-            var allSongs = filteredSongs.ToList();
-            foreach (Song s in SongsList.GetRange(0, clickedIndex))
-            {
-                allSongs.Add(s);
-            }        
-
-            mediaPlaybackList.Items.Clear();
-
-            // Add each song in new list to playbacklist
-            foreach (Song music in allSongs)
-            {
-                var mediaPlaybackItem = new MediaPlaybackItem(MediaSource.CreateFromUri(new Uri(BaseUri, music.AudioFile)));
-                mediaPlaybackList.Items.Add(mediaPlaybackItem);
-            }
-
-            mediaPlaybackList.MaxPlayedItemsToKeepOpen = 3;
-
-            MyMediaPlayer.Source = mediaPlaybackList;
-            MyMediaPlayer.AutoPlay = true;
-
-            // Update display of current song/artist/album
-            CurrentAlbumCover.Source = new BitmapImage(new Uri(BaseUri, song.AlbumCoverFile));
-            CurrentSong.Text = song.Name;
-            CurrentArtist.Text = song.Artist;
-        }
-
-        private void PlayPlaylist_ButtonClick(object sender, RoutedEventArgs e)
-        {
-            
-            foreach (Song music in SongsList)
-            {
-                var mediaPlaybackItem = new MediaPlaybackItem(MediaSource.CreateFromUri(new Uri(BaseUri, music.AudioFile)));
-                mediaPlaybackList.Items.Add(mediaPlaybackItem);
-            }
-
-            mediaPlaybackList.MaxPlayedItemsToKeepOpen = 3;
-
-            MyMediaPlayer.Source = mediaPlaybackList;
-            MyMediaPlayer.AutoPlay = true;            
-        }
-
-        private void prevButton_Click(object sender, RoutedEventArgs e)
-        {
-            mediaPlaybackList.MovePrevious();
-        }
-
-        private void nextButton_Click(object sender, RoutedEventArgs e)
-        {
-            mediaPlaybackList.MoveNext();
-        }
-
-        private void AppFunctionalitiesBottom_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Home_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Search_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void YourLibrary_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void CreatePlaylist_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void LikedSongs_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-  
-        #region Test saving to/loading from user's local files
-        private async void onStartup()
-        {
-            await loadSongs(Songs);            
-        }
-
-        private async Task loadSongs(ObservableCollection<Song> songs)
-        {
-           /* This is a path to my local computer, so pressing test button 4 will cause an error unless this
-            filepath is changed to your local path with a songstorage textfile. The textfile should have the 
-            format "songname;artistname;albumname;songname;artistname;albumname...". This stores the 
-            info for all the songs.*/
-            var file = await StorageFile.GetFileFromPathAsync(@"C:\Users\peter\Music\UWP_Music_Library\SongStorage.txt");
-
-            if (file != null)
-            {
-                // Reads all contents from storage text file
-                var allTextContents = await FileIO.ReadTextAsync(file);
-
-                // Splits contents into an array based on the ";" character
-                string[] allTextContentsArray = allTextContents.Split(';');
-
-                // Steps through array and instantiates new song class with the info.
-                int i = 0;
-                songs.Clear();
-                while (i < allTextContentsArray.Count())
-                {
-                    songs.Add(new Song(allTextContentsArray[i], allTextContentsArray[i + 1], allTextContentsArray[i + 2]));
-                    i += 3;
+                    MyMediaElement.SetSource(await storageFile.OpenAsync(FileAccessMode.Read), contentType);
+                    MyMediaElement.Play();
                 }
             }
-            else
-            {
-                throw new Exception();
-            }
         }
+    }
 
-        private async void saveSongAwait(string name, string artist, string album)
-        {
-             await saveSong(name, artist, album);
-        }
-           
-        private async Task saveSong(string name, string artist, string album)
-        {
-            /* This is a path to my local computer, so pressing test button 3 will cause an error unless this
-            filepath is changed to your local path with a songstorage textfile. The textfile should have the 
-            format "songname;artistname;albumname;songname;artistname;albumname...". This stores the 
-            info for all the songs.*/
-            var file = await StorageFile.GetFileFromPathAsync(@"C:\Users\peter\Music\UWP_Music_Library\SongStorage.txt");
+    private void MusicGridView_DragOver(object sender, DragEventArgs e)
+    {
+        e.AcceptedOperation = DataPackageOperation.Copy;
 
-            if (file != null)
-            {
-                //Adds all info from inputs to text file, splitting the info by the ";" character
-                string textOutput = $";{name};{artist};{album}";
-                await FileIO.AppendTextAsync(file, textOutput);
-            }
-            else
-            {
-                throw new Exception();
-            }
-        }
-
-        private async void Test3_Click(object sender, RoutedEventArgs e)
-        {
-            // This is a test song addition. It will need to have files of the same name in the Assets folders to work
-            saveSongAwait("Your Hand in Mine", "Explosions in the Sky", "The Earth Is Not a Cold Dead Place");
-            await loadSongs(Songs);
-        }
-
-        private void Test4_Click(object sender, RoutedEventArgs e)
-        {
-            onStartup();
-        }
-        #endregion
-
-        private void SortBySong_Click(object sender, RoutedEventArgs e)
-        {
-            MusicListView.ItemsSource = Songs.OrderBy(song => song.Name).ToList();
-        }
-
-        private void SortByAlbum_Click(object sender, RoutedEventArgs e)
-        {
-            MusicListView.ItemsSource = Songs.OrderBy(song => song.Album).ToList();
-        }
-
-        private void SortByArtist_Click(object sender, RoutedEventArgs e)
-        {
-            MusicListView.ItemsSource = Songs.OrderBy(song => song.Artist).ToList();
-
-        }
+        e.DragUIOverride.Caption = "drop to create a custom sound and tile";
+        e.DragUIOverride.IsCaptionVisible = true;
+        e.DragUIOverride.IsContentVisible = true;
+        e.DragUIOverride.IsGlyphVisible = true;
     }
 }
